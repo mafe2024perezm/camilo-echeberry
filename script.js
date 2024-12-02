1d20c32937bc6be857b599fc7e16de3dcfbdfbaf
mafe2024perezm <mperezmontealegre321@gmail.com>
2024-12-01 23:05:05 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let likeCount = 0;
-
-document.getElementById("like-button").addEventListener("click", () => {
-  likeCount++;
-  document.getElementById("like-count").textContent = `Likes: ${likeCount}`;
-});
